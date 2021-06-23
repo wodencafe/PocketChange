6b6c3d204ce938b0d033ce30ff4a2be59f193f41
Woden Cafe <wodencafe@woden.cafe>
2021-06-23 10:48:16 -0500

    Initial Project

---
+++ lib/src/main/java/cafe/woden/change/Coin.java
@@ -0,0 +1,156 @@
+
+package cafe.woden.change;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Coin
+{
+	DOLLAR_COIN( 100, 0.2 ), HALF_DOLLAR( 50, 0.2 ), QUARTER( 25, 0.4 ), DIME( 10, 0.5 ), NICKEL( 5, 0.5 ), PENNY( 1, 0.8 );
+
+	private int value;
+
+	private double probability;
+
+	private boolean heads;
+
+	private Coin( int value, double probability )
+	{
+		this.value = value;
+		this.probability = probability;
+		heads = flipLogic();
+	}
+
+	public void flip()
+	{
+		heads = flipLogic();
+	}
+
+	private static boolean flipLogic()
+	{
+		double flipValue = Math.random();
+		if ( flipValue >= 0.5 )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public static Collection<Coin> getTotalValue( int total )
+	{
+		List<Coin> returnedCoins = new ArrayList<>();
+		List<Coin> coins = new ArrayList<>( Arrays.asList( Coin.values() ) );
+		int currentTotal = 0;
+		while ( currentTotal < total )
+		{
+			Collections.shuffle( coins );
+			Set<Coin> coinsToRemove = new HashSet<>();
+			for ( Coin coin : coins )
+			{
+				if ( coin.value + total >= (total - currentTotal) )
+				{
+					coinsToRemove.add( coin );
+				}
+			}
+			for ( Coin coin : coinsToRemove )
+			{
+
+				coins.remove( coin );
+			}
+			Coin coin = get( coins );
+			if ( coins.isEmpty() && coin.value + currentTotal > total )
+			{
+				return returnedCoins;
+			}
+			currentTotal += coin.value;
+			if ( currentTotal > total )
+			{
+				throw new RuntimeException( "Whoops" );
+			}
+			returnedCoins.add( coin );
+		}
+		Collections.shuffle( returnedCoins );
+		return returnedCoins;
+	}
+
+	public static Set<Coin> getTotalCount( int count )
+	{
+		Stream<Coin> infiniteCoins = Stream.generate( Coin::get );
+		return infiniteCoins.limit( count )
+			.collect( Collectors.toSet() );
+	}
+
+	public static Coin get( Collection<Coin> coins )
+	{
+		try
+		{
+			double p = SecureRandom.getInstanceStrong()
+				.doubles( 0.0, 1.0 )
+				.findAny()
+				.getAsDouble();
+			double cumulativeProbability = 0.0;
+			for ( Coin coin : coins )
+			{
+				cumulativeProbability += coin.probability;
+				if ( p <= cumulativeProbability )
+				{
+					return coin;
+				}
+			}
+			return get();
+
+		}
+		catch ( Throwable th )
+		{
+			throw new RuntimeException( th );
+		}
+	}
+
+	public static Coin get()
+	{
+		try
+		{
+			double p = SecureRandom.getInstanceStrong()
+				.doubles( 0.0, 1.0 )
+				.findAny()
+				.getAsDouble();
+			double cumulativeProbability = 0.0;
+			Set<Coin> coins = new HashSet<>( Arrays.asList( Coin.values() ) );
+			for ( Coin coin : coins )
+			{
+				cumulativeProbability += coin.probability;
+				if ( p <= cumulativeProbability )
+				{
+					return coin;
+				}
+			}
+			return get();
+		}
+		catch ( Throwable th )
+		{
+			throw new RuntimeException( th );
+		}
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	public boolean isHeads()
+	{
+		return heads;
+	}
+}
---
+++ lib/src/main/java/cafe/woden/change/Main.java
@@ -0,0 +1,83 @@
+
+package cafe.woden.change;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main
+{
+	private static int TOTAL_VALUE;
+	static
+	{
+		try
+		{
+			TOTAL_VALUE = SecureRandom.getInstanceStrong()
+				.ints( 100, 200 )
+				.findAny()
+				.getAsInt();
+		}
+		catch ( NoSuchAlgorithmException e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static void main( String[] args )
+	{
+		Collection<Coin> tailsCoins = new ArrayList<>();
+		Collection<Coin> allCoins = Coin.getTotalValue( TOTAL_VALUE );
+		for ( Coin coin : allCoins )
+		{
+			coin = shuffleCoin( coin );
+			if ( !coin.isHeads() )
+			{
+				tailsCoins.add( coin );
+			}
+		}
+		int totalCoinValue = 0;
+		for ( Coin coin : Coin.values() )
+		{
+			Collection<Coin> coins = new ArrayList<>();
+			for ( Coin tailsCoin : tailsCoins )
+			{
+				if ( coin == tailsCoin )
+				{
+					coins.add( coin );
+				}
+			}
+			int totalValue = coins.stream()
+				.map( Coin::getValue )
+				.reduce( 0, Integer::sum );
+			System.out.println( "Coin: " + coin.toString() + " Count: " + coins.size() + " Value: " + totalValue );
+			totalCoinValue += totalValue;
+		}
+		double totalCoinValueDouble;
+		if ( totalCoinValue > 0 )
+		{
+			totalCoinValueDouble = totalCoinValue * 1.0 / 100;
+		}
+		else
+		{
+			totalCoinValueDouble = 0;
+		}
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		String moneyString = formatter.format( totalCoinValueDouble );
+		System.out.println( "Total value: " + moneyString );
+	}
+
+	private static Coin shuffleCoin( Coin coin )
+	{
+		int shuffleSize = ThreadLocalRandom.current()
+			.nextInt( 1, 100 );
+		for ( int x = 0; x < shuffleSize; x++ )
+		{
+			coin.flip();
+		}
+		return coin;
+	}
+
+}
