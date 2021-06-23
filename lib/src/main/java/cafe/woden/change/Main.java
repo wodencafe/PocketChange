48051e65925fd09f9f9b8ad8236773c23b85c675
Woden Cafe <wodencafe@woden.cafe>
2021-06-23 14:21:10 -0500

    Fix some logic, use randomization more

--- lib/src/main/java/cafe/woden/change/Main.java
+++ lib/src/main/java/cafe/woden/change/Main.java
@@ -1,24 +1,31 @@
 
 package cafe.woden.change;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class Main
 {
-	private static int TOTAL_VALUE;
-	static
+	private static int getTotalValue()
 	{
 		try
 		{
 			SecureRandom random = SecureRandom.getInstanceStrong();
 			random.setSeed( Util.getRandomNumber()
 				.longValue() );
-			TOTAL_VALUE = random.ints( 100, 200 )
+			return random.ints( 100, 200 )
 				.findAny()
 				.getAsInt();
 		}
@@ -30,8 +37,62 @@ public class Main
 
 	public static void main( String[] args )
 	{
+		getOutput();
+		//System.out.println( "Coin: " + coin.toString() + " Count: " + coins.size() + " Value: " + totalValue );
+		//writeOutput( Paths.get( "/home/cboyd/200times.txt" ), 1000 );
+	}
+
+	private static void writeOutput( Path path, int number )
+	{
+		try
+		{
+			if ( Files.exists( path ) )
+			{
+				Files.delete( path );
+			}
+
+		}
+		catch ( Throwable th )
+		{
+			throw new RuntimeException( th );
+		}
+
+		try
+		{
+			if ( !Files.exists( path ) )
+			{
+				Files.createFile( path );
+			}
+			AtomicInteger x = new AtomicInteger();
+			IntStream.generate( Main::getOutput )
+				.limit( number )
+				.forEach( output -> {
+					try
+					{
+						byte[] bytesToWrite = (String.valueOf( output ) + ((x.get() + 1) == number ? "" : ", ")).getBytes();
+						Files.write( path, bytesToWrite, StandardOpenOption.APPEND );
+					}
+					catch ( IOException e )
+					{
+						throw new RuntimeException( e );
+					}
+					x.incrementAndGet();
+				} );
+
+			Files.write( path, System.lineSeparator()
+				.getBytes(), StandardOpenOption.APPEND );
+		}
+		catch ( Throwable th )
+		{
+			throw new RuntimeException( th );
+		}
+	}
+
+	private static int getOutput()
+	{
+
 		Collection<Coin> tailsCoins = new ArrayList<>();
-		Collection<Coin> allCoins = Coin.getTotalValue( TOTAL_VALUE );
+		Collection<Coin> allCoins = Coin.getTotalValue( getTotalValue() );
 		for ( Coin coin : allCoins )
 		{
 			coin = shuffleCoin( coin );
@@ -56,6 +117,7 @@ public class Main
 				.reduce( 0, Integer::sum );
 			System.out.println( "Coin: " + coin.toString() + " Count: " + coins.size() + " Value: " + totalValue );
 			totalCoinValue += totalValue;
+
 		}
 		double totalCoinValueDouble;
 		if ( totalCoinValue > 0 )
@@ -69,21 +131,26 @@ public class Main
 		NumberFormat formatter = NumberFormat.getCurrencyInstance();
 		String moneyString = formatter.format( totalCoinValueDouble );
 		System.out.println( "Total value: " + moneyString );
+		return totalCoinValue;
+
 	}
 
 	private static Coin shuffleCoin( Coin coin )
 	{
 		try
 		{
-			int shuffleSize = SecureRandom.getInstanceStrong()
-				.ints( SecureRandom.getInstanceStrong()
-					.ints( 1, 100 )
+			SecureRandom random = SecureRandom.getInstanceStrong();
+
+			random.setSeed( Util.getRandomNumber()
+				.longValue() );
+			int shuffleSize = random.ints( SecureRandom.getInstanceStrong()
+				.ints( 1, 100 )
+				.findAny()
+				.getAsInt(),
+				SecureRandom.getInstanceStrong()
+					.ints( 100, 1000 )
 					.findAny()
-					.getAsInt(),
-					SecureRandom.getInstanceStrong()
-						.ints( 100, 1000 )
-						.findAny()
-						.getAsInt() )
+					.getAsInt() )
 				.findAny()
 				.getAsInt();
 			for ( int x = 0; x < shuffleSize; x++ )
